aed2eacf80eb5d0901301b9ded801fac1981779a
Евгений Дружняев <druzhniaev@gmail.com>
2019-04-19 18:01:03 +0600

    lesson-1

---
+++ lesson-1/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let money = prompt('Ваш бюджет на месяц?', ''),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false
+};
+
+let exp1 = prompt('Введите обязательную статью расходов в этом месяце','');
+appData.expenses[exp1] = prompt('Во сколько обойдется?', '');
+
+let exp2 = prompt('Введите обязательную статью расходов в этом месяце','');
+appData.expenses[exp2] = prompt('Во сколько обойдется?', '');
+
+// Бюджет на 1 день
+alert( money/30 ); 
+
+
+
+console.log(appData);
