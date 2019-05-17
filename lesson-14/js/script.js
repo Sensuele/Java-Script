efaa2f804bf7bc8be66c3bbe1250cec59532e0c0
Евгений Дружняев <druzhniaev@gmail.com>
2019-05-17 16:32:21 +0600

    урок 14

--- lesson-14/js/script.js
+++ lesson-14/js/script.js
@@ -1,3 +1,22 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
+  // Расписание туров 
+	$('.sheldure').on('click', () => {
+		$('.overlay').fadeToggle('slow');
+		$('.modal').slideDown("slow");
+  });
+  // Получить консультацию
+	$('.main_btn').on('click', () => {
+		$('.overlay').fadeToggle('slow');
+		$('.modal').slideDown("slow");
+  });
+  // выбрать тур
+	$('.main_btna').on('click', () => {
+		$('.overlay').fadeToggle('slow');
+		$('.modal').slideDown("slow");
+  });
+  // закрытие модального окна
+	$('.close').on('click', () => {
+		$('.overlay').fadeToggle('slow');
+		$('.modal').slideUp("slow");
+	});
 });
